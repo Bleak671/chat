a19bd0dd36a3390b113d5a7abde47a20ce6913ba
Maksim Odinochenko <maksim.odinochenko@yandex.ru>
2020-04-23 19:22:07 +0300

    ...

---
+++ KSIS_3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace KSIS_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listChat = new System.Windows.Forms.ListBox();
+            this.list = new System.Windows.Forms.ListBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(470, 307);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 20);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 307);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listChat
+            // 
+            this.listChat.FormattingEnabled = true;
+            this.listChat.Location = new System.Drawing.Point(188, 10);
+            this.listChat.Name = "listChat";
+            this.listChat.Size = new System.Drawing.Size(357, 290);
+            this.listChat.TabIndex = 4;
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(22, 10);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(147, 160);
+            this.list.TabIndex = 5;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Enabled = false;
+            this.btnFind.Location = new System.Drawing.Point(41, 203);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(112, 23);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "Поиск абонентов";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Имя";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 340);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.listChat);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Одногруппники";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listChat;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ KSIS_3/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace KSIS_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool conn = false;
+        public List<Client> clients = new List<Client>();
+        public class Client
+        {
+            public IPEndPoint iep;
+            public String name;
+            public Client(IPEndPoint iepp, string str)
+            {
+                iep = iepp;
+                name = str;
+            }
+        }
+
+        class Packet
+        {
+            public int type;
+            public int length;
+            public byte[] data;
+
+            public Packet(int typ)
+            {
+                type = typ;
+                length = 2 * sizeof(int);
+                data = null;
+            }
+
+            public Packet(int typ, int len, byte[] dat)
+            {
+                type = typ;
+                length = len + 2 * sizeof(int);
+                data = dat;
+            }
+
+            public Packet(byte[] dat)
+            {
+                type = BitConverter.ToInt32(dat, 0);
+                length = BitConverter.ToInt32(dat, sizeof(int));
+                Buffer.BlockCopy(dat, 2 * sizeof(int), data, 0, length - 2 * sizeof(int));
+            }
+            public byte[] getBytes()
+            {
+                byte[] data = new byte[length];
+                Buffer.BlockCopy(BitConverter.GetBytes(type), 0, data, 0, sizeof(int));
+                Buffer.BlockCopy(BitConverter.GetBytes(length), 0, data, sizeof(int), sizeof(int));
+                if (data != null)
+                    Buffer.BlockCopy(data, 0, data, 2 * sizeof(int), length - 2 * sizeof(int));
+                return data;
+            }
+        }
+
+        public static String GetName(IPEndPoint iep, List<Client> list)
+        {
+            foreach(Client c in list)
+            {
+                if (IPEndPoint.Equals(c.iep,iep))
+                {
+                    return c.name;
+                }
+            }
+            return null;
+        }
+
+        private void Connect()
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("230.230.230.230"), 228);
+            try
+            {
+                Packet msg = new Packet(0, Encoding.Unicode.GetBytes(textBox2.Text).Length, Encoding.Unicode.GetBytes(textBox2.Text));
+                client.Send(msg.getBytes(), msg.length, iep);
+                client.Close();
+
+                if (!conn)
+                {
+                    Task UdpRec = new Task(UdpReceive);
+                    Task TcpRec = new Task(TcpReceive);
+                    UdpRec.Start();
+                    TcpRec.Start();
+                }
+                conn = true;
+                MessageBox.Show("Вы подключились к чату");
+            }
+            catch (Exception ex)
+            {
+                client.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void UdpReceive()
+        {
+            UdpClient client = new UdpClient(228);
+            client.JoinMulticastGroup(IPAddress.Parse("230.230.230.230"), 50);
+            IPEndPoint iep = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = client.Receive(ref iep);
+                    Packet msg = new Packet(data);
+                    MessageBox.Show(Encoding.Unicode.GetString(msg.data));
+                    switch (msg.type)
+                    {
+                        case 0:
+                            bool flag = true;
+                            foreach (Client c in clients)
+                            { 
+                                if (clients.Contains(c))
+                                {
+                                    flag = false;
+                                }
+                            }
+                            if (flag)
+                            {
+                                Client c = new Client(iep, Encoding.Unicode.GetString(msg.data));
+                                clients.Add(c);
+                            }
+                            TcpAnswer();
+                            break;
+                        case 1:
+                            foreach (Client c in clients)
+                            {
+                                if (clients.Contains(c))
+                                {
+                                    clients.Remove(c);
+                                    listChat.Items.Add(c.name + " покинул чат");
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private void TcpAnswer()
+        {
+            IPEndPoint remoteIp = null;
+            TcpClient sender = new TcpClient(remoteIp);
+            NetworkStream stream = sender.GetStream();
+            Packet msg = new Packet(Encoding.Unicode.GetBytes(textBox2.Text));
+            stream.Write(msg.getBytes(), 0, msg.length);
+            stream.Close();
+            sender.Close();
+        }
+
+        private void TcpReceive()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse(GetLocalIPAddress()), 50);
+            listener.Start();
+            byte[] data = new byte[65536];
+
+            while (conn)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                stream.Read(data, 0, data.Length);
+                Packet msg = new Packet(data);
+                IPEndPoint iep = (IPEndPoint)client.Client.RemoteEndPoint;
+                switch (msg.type)
+                {
+                    case 0:
+                        Client c = new Client(iep, Encoding.Unicode.GetString(msg.data));
+                        if (!clients.Contains(c))
+                        {
+                            clients.Add(c);
+                            listChat.Items.Add(GetName(iep, clients) + " присоединился");
+                        }
+                        break;
+                    case 1:
+                        listChat.Items.Add(GetName(iep,clients) + ":" + Encoding.Unicode.GetString(msg.data));
+                        break;
+                }
+            }
+            
+            listener.Stop();
+        }
+
+        private void LeaveChat()
+        {
+            conn = false;
+            UdpClient client = new UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("230.230.230.230"), 228);
+            try
+            {
+                Packet msg = new Packet(1);
+                client.Send(msg.getBytes(), msg.length, ep);
+                client.Close();
+                MessageBox.Show("Вы отключились");
+            }
+            catch (Exception ex)
+            {
+                client.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (String.Compare(textBox2.Text, "") != 0)
+            {
+                btnFind.Enabled = true;
+            }
+        }
+
+        private void btnLeave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("230.230.230.230"), 228);
+            try
+            {
+                Packet msg = new Packet(1);
+                client.Send(msg.getBytes(), msg.length, ep);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
